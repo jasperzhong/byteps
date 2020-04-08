4c89c7ff684c1dfafcdc734556e4dd7ecfcac1ef
zhongyuchen <izhongyuchen@gmail.com>
2020-04-08 10:04:11 +0800

    mxnet: fix

--- example/mxnet/train_gluon_imagenet_byteps_gc.py
+++ example/mxnet/train_gluon_imagenet_byteps_gc.py
@@ -170,7 +170,7 @@ def main():
         LRScheduler('linear', base_lr=opt.lr, target_lr=opt.lr * nworker / ngpus / 2,
                     nepochs=opt.warmup_epochs, iters_per_epoch=num_batches),
         LRScheduler(opt.lr_mode, base_lr=opt.lr * nworker / ngpus / 2, target_lr=0,
-                    nepochs=opt.num_epochs - opt.warmup_epochs,
+                    nepochs=opt.num_epochs - opt.warmup_epochs - opt.resume_epoch,
                     iters_per_epoch=num_batches,
                     step_epoch=lr_decay_epoch,
                     step_factor=lr_decay, power=2)
