6fd14d413f7de13c2225802fd261edc9efd6c114
zhongyuchen <izhongyuchen@gmail.com>
2020-05-13 14:59:49 +0800

    topk: init commit

--- byteps/common/compressor/strategy/topk.h
+++ byteps/common/compressor/strategy/topk.h
@@ -36,7 +36,19 @@ class TopkCompressor : public BaseCompressor {
                   ByteBuf& decompressed) override;
 
  private:
+  size_t Packing(const void* src, size_t size, int dtype);
+
+  template <typename index_t, typename scalar_t>
+  size_t _Packing(index_t* dst, const scalar_t* src, size_t len);
+
+  size_t Unpacking(void* dst, const void* src, size_t size, int dtype);
+  
+  template <typename index_t, typename scalar_t>
+  size_t _Unpacking(scalar_t* dst, const index_t* src, size_t len);
+
+ private:
   int _k;
+  int _src_len;
 };
 }  // namespace compressor
 }  // namespace common
