9e562c203052b4ec59572eda558227ca1ee71180
zhongyuchen <izhongyuchen@gmail.com>
2020-08-23 22:16:55 +0800

    discard last batch

--- example/mxnet/train_gluon_imagenet_byteps_gc.py
+++ example/mxnet/train_gluon_imagenet_byteps_gc.py
@@ -444,6 +444,8 @@ def main():
 
             for i, batch in enumerate(train_data):
                 data, label = batch_fn(batch, ctx)
+                if data.shape[0] != batch_size:
+                    continue
 
                 if opt.mixup:
                     lam = np.random.beta(opt.mixup_alpha, opt.mixup_alpha)
