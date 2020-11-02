111b689cf10d905ea152229609392b7819d377d5
zhongyuchen <izhongyuchen@gmail.com>
2020-11-02 22:03:30 +0800

    debug

--- byteps/mxnet/__init__.py
+++ byteps/mxnet/__init__.py
@@ -335,6 +335,8 @@ class DistributedTrainer(mx.gluon.Trainer):
             self._f.write(ba)
             self._f.flush()
 
+        before = []
+        after = []
         for i, param in enumerate(self._params):
             if param.grad_req != 'null':
                 # normalized with batch_size and num_workers
@@ -342,10 +344,19 @@ class DistributedTrainer(mx.gluon.Trainer):
                     param._grad[0], 1.0 / self._scale / self._bps_size, out=param._grad[0])
                 compressed, ctx = self._intra_compressors[param.name].compress(
                     param._grad[0])
+                before.append(compressed)
                 byteps_push_pull(compressed, is_average=False,
                                  name="gradient_" + str(i), priority=-i)
                 param._grad[0][:] = self._intra_compressors[param.name].decompress(
                     compressed, ctx, x=param._data[0])
+                after.append(param._grad[0])
+
+        for i, front, tail in enumerate(zip(before, after)):
+            front = front.asnumpy().flatten()
+            tail = tail.asnumpy().flatten()
+            diff = np.abs(front - tail)
+            print("param %d: max norm=%.2f",
+                  (i, np.linalg.norm(diff, ord=np.inf)))
 
     def _init_params(self):
         tensors = []
