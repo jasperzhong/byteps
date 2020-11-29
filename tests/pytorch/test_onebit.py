923aa732efc21982440757dcb3e071c19661d13a
zhongyuchen <izhongyuchen@gmail.com>
2020-11-29 17:08:01 +0800

    add test

---
+++ tests/pytorch/test_onebit.py
@@ -0,0 +1,129 @@
+# Copyright 2020 Amazon Technologies, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import itertools
+import unittest
+
+import byteps.torch as bps
+import numpy as np
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+from parameterized import parameterized
+from tqdm import tqdm
+
+from meta_test import MetaTest
+from utils import fake_data
+
+
+def onebit(x, scaling):
+    if scaling:
+        l1 = np.linalg.norm(x.flatten(), 1)
+    sign = x < 0
+    sign = -((sign << 1) - 1)
+    if scaling:
+        return l1 / len(x.flatten()) * sign
+    else:
+        return sign
+
+
+class OnebitTestCase(unittest.TestCase, metaclass=MetaTest):
+    TEST_BENCH = [
+        [True, False],
+        ["float32", "float16"]
+    ]
+
+    @parameterized.expand(itertools.product(*TEST_BENCH))
+    def test_onebit(self, scaling, dtype):
+        bps.init()
+        device = torch.device(
+            "cuda") if torch.cuda.is_available() else torch.device("cpu")
+        net = torchvision.models.resnet18().to(device)
+        net = net.type(dtype)
+
+        # hyper-params
+        batch_size = 32
+        lr = 0.01
+        compression_params = {
+            "compressor": "onebit",
+            "scaling": scaling,
+            "fp16": True if dtype == "float16" else False
+        }
+
+        optimizer = optim.SGD(net.parameters(), lr=lr)
+        optimizer = bps.DistributedOptimizer(
+            optimizer, named_parameters=net.named_parameters(),
+            compression_params=compression_params)
+
+        train_data = fake_data(batch_size=batch_size)
+
+        params = {}
+
+        for i, param_group in enumerate(optimizer.param_groups):
+            for param in param_group['params']:
+                if param.requires_grad:
+                    params[param] = param.data.cpu().numpy()
+
+        for it, batch in tqdm(enumerate(train_data)):
+            data = batch[0].to(device).type(dtype)
+            label = batch[1].to(device)
+
+            output = net(data)
+            loss = F.cross_entropy(output, label)
+            optimizer.zero_grad()
+            loss.backward()
+
+            gs = {}
+            xs = {}
+
+            for i, param_group in enumerate(optimizer.param_groups):
+                for param in param_group['params']:
+                    if param.requires_grad:
+                        gs[param] = param.grad.cpu().numpy()
+                        xs[param] = param.data.cpu().numpy()
+
+            optimizer.step()
+
+            for i, param_group in enumerate(optimizer.param_groups):
+                for param in param_group['params']:
+                    if param.requires_grad:
+                        g = gs[param] / (batch_size * bps.size())
+                        c = onebit(g, scaling)
+
+                        cs = onebit(c, scaling)
+                        c = cs
+
+                        params[param] -= lr * c
+
+        cnt = 0
+        tot = 0
+        threshold = 0 if dtype == "float32" else 10
+        for i, param_group in enumerate(optimizer.param_groups):
+            for param in param_group['params']:
+                if param.requires_grad:
+                    x = param.data.cpu().numpy()
+                    tot += len(x.flatten())
+                    if not np.allclose(params[i], x, atol=np.finfo(dtype).eps):
+                        diff = np.abs(x.flatten() - params[i].flatten())
+                        idx = np.where(diff > np.finfo(dtype).eps)
+                        cnt += len(idx[0])
+
+        assert cnt <= threshold, "false/tot=%d/%d=%f" % (
+            cnt, tot, cnt/tot)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/pytorch/utils.py
@@ -0,0 +1,49 @@
+import torch
+import numpy as np
+from numba import jit
+
+
+def fake_data(dtype=torch.float32, batch_size=32, height=224, width=224, depth=3, num_classes=1000):
+    image_list = []
+    label_list = []
+    for _ in range(8):
+        image = torch.random.normal(-1, 1,
+                                    size=[1, depth, height, width],
+                                    dtype=dtype)
+        label = torch.randint(0, num_classes, size=[1, 1])
+
+        images = image.repeat_interleave(128, dim=0)
+        labels = label.repeat_interleave(128, dim=0)
+        # print(labels)
+        image_list.append(images)
+        label_list.append(labels)
+
+    images = torch.cat(image_list, dim=0)
+    labels = torch.cat(label_list, dim=0)
+
+    return images, labels
+
+
+@ jit(nopython=True)
+def xorshift128p(state):
+    t = state[0]
+    s = state[1]
+    state[0] = s
+    t ^= t << np.uint64(23)
+    t ^= t >> np.uint64(17)
+    t ^= s ^ (s >> np.uint64(26))
+    state[1] = t
+    return np.uint64(t + s)
+
+
+@ jit(nopython=True)
+def bernoulli(p, state):
+    t = p * np.iinfo(np.uint64).max
+    r = np.array([xorshift128p(state)
+                  for _ in range(len(p))], dtype=np.float32)
+    return r < t
+
+
+@ jit(nopython=True)
+def randint(low, high, state):
+    return xorshift128p(state) % (high - low) + low
